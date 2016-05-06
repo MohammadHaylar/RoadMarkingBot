c11cbcbf3bd494ac15b3dc42dac54529db455503
Haylar <Haylar051@Gmail.com>
2016-05-06 14:42:01 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {Markeingi},
+    sudo_users = {@Markeingi},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
